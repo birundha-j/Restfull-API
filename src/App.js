c7f101cd61942a5f1604b626dd53dc8c6df8ada7
ramya vbr <birundhajayaraman2606@gmail.com>
2021-07-05 15:52:50 +0530

    finish Assessment

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import { useDispatch, connect } from "react-redux";
 import { addTask, getAddtask, updateTask, deleteTask } from './Redux/Action/taskAction';
 import Bell from '../src/Images/bell.svg';
 import OkIcon from '../src/Images/okicon.svg';
-import Edit from './Images/EditIcon.svg';
-import Remove from './Images/delete.svg';
+import Remove from './Images/remove.svg';
 import User from './Images/user.svg';
 
 const AccessToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2MjUyNDAzMTEsIm5iZiI6MTYyNTI0MDMxMSwianRpIjoiZjNlYjgzYTgtMjM4Ny00ZDM5LWIwYmEtNDZlZmU1MGZhZThlIiwiaWRlbnRpdHkiOnsibmFtZSI6IlN1YmkgU2lyIiwiZW1haWwiOiJzbWl0aGNoZXJ5bEB5YWhvby5jb20iLCJ1c2VyX2lkIjoidXNlcl82YmVlYzQ1OTkxNWY0NTA3YThkMjUyMGU2MGUwM2MzZSIsImNvbXBhbnlfaWQiOiJjb21wYW55XzNjNjhjZDk0ZWJkNjQ4Yzc4ZDc2ODcyY2ZhOWY4Y2ZiIiwiaWNvbiI6Imh0dHA6Ly93d3cuZ3JhdmF0YXIuY29tL2F2YXRhci9mMmU5YWNkZWM4MTdlMjRkMjk4MGQ4NTNlODkzODVmNT9kZWZhdWx0PWh0dHBzJTNBJTJGJTJGczMuc2xvb3ZpLmNvbSUyRmF2YXRhci1kZWZhdWx0LWljb24ucG5nIiwiYnlfZGVmYXVsdCI6Im91dHJlYWNoIn0sImZyZXNoIjpmYWxzZSwidHlwZSI6ImFjY2VzcyJ9.pQSfEzNzYv_IVtPFkUKFucl1SSIqpmKnx4Jlxdhi7IY";
@@ -113,121 +112,133 @@ function App(props) {
       return (
         task[data].value = ""
       )
-  });
-  setTask((prevState) => ({
-    ...prevState,
-  }));
-};
+    });
+    setTask((prevState) => ({
+      ...prevState,
+    }));
+    setShowtask(false)
+    setShowAddtask(true)
+  };
 
-const handlemouseEnter = (id) => {
-  setShowEdit(id)
-}
+  const handlemouseEnter = (id) => {
+    setShowEdit(id)
+  }
 
-const editTasklist = (id) => {
-  setIndexrow(id)
-  setRemove(true)
-  setShowtask(true)
-  setShowAddtask(false)
-  setUpdatebtn(true)
+  const editTasklist = (id) => {
+    setIndexrow(id)
+    setRemove(true)
+    setShowtask(true)
+    setShowAddtask(false)
+    setUpdatebtn(true)
 
-  let customtime = new Date(taskList[id].task_time * 1000).toISOString().substr(11, 8)
+    let customtime = new Date(taskList[id].task_time * 1000).toISOString().substr(11, 8)
 
-  task.description.value = taskList[id].task_msg
-  task.date.value = taskList[id].task_date
-  task.time.value = customtime
-  task.assignuser.value = taskList[id].assigned_user
+    task.description.value = taskList[id].task_msg
+    task.date.value = taskList[id].task_date
+    task.time.value = customtime
+    task.assignuser.value = taskList[id].assigned_user
 
-  setTask((prevState) => ({
-    ...prevState,
+    setTask((prevState) => ({
+      ...prevState,
 
-  }));
-}
+    }));
+  }
 
-const deletedTask = () => {
-  dispatch(deleteTask(taskList[indexrow].id)).then(res => {
-    alert("Are you sure you want to delete this task")
-    handleCancel()
-  })
-  setShowtask(false)
-  setShowAddtask(true)
-}
+  const deletedTask = () => {
+    dispatch(deleteTask(taskList[indexrow].id)).then(res => {
+      alert("Are you sure you want to delete this task")
+      handleCancel()
+    })
+    setShowtask(false)
+    setShowAddtask(true)
+  }
 
 
-return (
-  <div className="container">
-    <div className="taskDescription">
-      <div className="descriptionBox">TASK  {taskList.length} </div>
-      <div className="btnAdd" onClick={addTasks}>+</div>
-    </div>
-
-    {showtask &&
-      <div className="taskFields">
-
-        <div className="fieldView" >
-          <label className="taskName">Task Description</label>
-          <input className="tasknameInput" onChange={(e) => checkValidation(e, "description")} value={task.description.value} />
+  return (
+    <div className="container">
+      <div className="taskDescription">
+        <div className="descriptionBox">TASKS  {taskList.length} </div>
+        <div className="btnAdd tooltip" onClick={addTasks}>+
+          <span className="tooltiptext">New Task</span>
         </div>
-
-        <div className="dateTime">
-          <div className="fielddateView">
-            <label className="taskName">Date</label>
-            <input className="taskDate" type="date" onChange={(e) => checkValidation(e, "date")} value={task.date.value} />
-          </div>
-          <div className="fielddateView">
-            <label className="taskName">Time</label>
-            <input className="taskTime" type="time" step="2" onChange={(e) => checkValidation(e, "time")} value={task.time.value} />
-          </div>
-        </div>
-
-        <div className="fieldView" >
-          <label className="taskName">Assign User</label>
-          <select className="tasknameInput" onChange={(e) => checkValidation(e, "assignuser")} value={task.assignuser.value} >
-            {assignuser.map((val) => {
-              return (
-                <option >{val.value}</option>
-              )
-            })}
-          </select >
-        </div>
-
-        <div className="customBtn">
-          {remove && <img alt="Girl in a jacket" src={Remove} className="removeIcon" onClick={deletedTask} />}
-          <div className="btn_view">
-            <button className="cancelBtn" onClick={handleCancel}>Cancel</button>
-            <button className="saveBtn" onClick={() => onsubmit(updatebtn ? "update" : "")}>Save</button>
-          </div>
-        </div>
-
       </div>
-    }
-    {showAddtask &&
-      <>
-        {taskList.map((data, index) => {
-          return (
-            <div className="addingTask" onMouseLeave={() => setShowEdit()}>
 
-              <div className="prof_Name">
-                <img alt="Girl in a jacket" src={User} className="img_view" />
-                <div>
-                  <div>{data.task_msg}</div>
-                  <div>{data.task_date}</div>
-                </div>
-              </div>
+      {showtask &&
+        <div className="taskFields">
 
-              <div className="taskicons"  >
-                {showEdit === index ? <img alt="Girl in a jacket" src={Edit} className="editIcon" onClick={() => editTasklist(index)} /> : null}
-                <img alt="Girl in a jacket" src={Bell} className="bellIcon" onMouseEnter={() => handlemouseEnter(index)} />
-                <img alt="Girl in a jacket" src={OkIcon} className="okIcon" />
-              </div>
+          <div className="fieldView" >
+            <label className="taskName">Task Description</label>
+            <input className="tasknameInput" onChange={(e) => checkValidation(e, "description")} value={task.description.value} />
+          </div>
 
+          <div className="dateTime">
+            <div className="fielddateView">
+              <label className="taskName">Date</label>
+              <input className="taskDate" type="date" onChange={(e) => checkValidation(e, "date")} value={task.date.value} />
             </div>
-          )
-        })
-        }
-      </>
-    }
-  </div>
-);
+            <div className="fielddateView">
+              <label className="taskName">Time</label>
+              <input className="taskTime" type="time" step="2" onChange={(e) => checkValidation(e, "time")} value={task.time.value} />
+            </div>
+          </div>
+
+          <div className="fieldView" >
+            <label className="taskName">Assign User</label>
+            <select className="tasknameInput" onChange={(e) => checkValidation(e, "assignuser")} value={task.assignuser.value} >
+              {assignuser.map((val) => {
+                return (
+                  <option >{val.value}</option>
+                )
+              })}
+            </select >
+          </div>
+
+          <div className="customBtn">
+            {remove && <div className="removeIcon tooltip" onClick={deletedTask}>
+              <img src={Remove} />
+              <span className="tooltiptext">Delete Task</span>
+            </div>}
+            <div className="btn_view">
+              <button className="cancelBtn" onClick={handleCancel}>Cancel</button>
+              <button className="saveBtn" onClick={() => onsubmit(updatebtn ? "update" : "")}>Save</button>
+            </div>
+          </div>
+
+        </div>
+      }
+      {showAddtask &&
+        <>
+          {taskList.map((data, index) => {
+            return (
+              <div className="addingTask" onMouseLeave={() => setShowEdit()}>
+
+                <div className="prof_Name">
+                  <img src={User} className="img_view" />
+                  <div>
+                    <div className="showTaskname">{data.task_msg}</div>
+                    <div className="showTaskdate">{`${new Date(data.task_date).getDate()}/${new Date(data.task_date).getMonth() + 1}/${new Date(data.task_date).getFullYear()}`}</div>
+                  </div>
+                </div>
+
+                <div className="taskicons"  >
+                  {showEdit === index ? <div className="editIcon tooltip" onClick={() => editTasklist(index)}><div className="editIconshow">✎</div>
+                    <span className="tooltiptext">Edit This Task</span>
+                  </div> : null}
+                  <div className="bellIcon tooltip" onMouseEnter={() => handlemouseEnter(index)}>
+                    <img src={Bell} />
+                    <span className="tooltiptext">Snooze this Task to appear in your at a later date</span>
+                  </div>
+                  <img src={OkIcon} className="okIcon" />
+                </div>
+
+              </div>
+            )
+          })
+          }
+        </>
+      }
+    </div>
+  );
 }
 
 const mapStateToProps = (state) =>
